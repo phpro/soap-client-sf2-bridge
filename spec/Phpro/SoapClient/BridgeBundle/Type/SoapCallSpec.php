3927bc554db6a2ac286c334c741074963db63fa3
vernija <jan.vernieuwe@phpro.be>
2015-11-17 09:43:14 +0100

    Added some more spec tests for the SoapCall class

--- spec/Phpro/SoapClient/BridgeBundle/Type/SoapCallSpec.php
+++ spec/Phpro/SoapClient/BridgeBundle/Type/SoapCallSpec.php
@@ -3,12 +3,16 @@
 namespace spec\Phpro\SoapClient\BridgeBundle\Type;
 
 use Phpro\SoapClient\BridgeBundle\Type\SoapCall;
+use Phpro\SoapClient\Client;
 use Phpro\SoapClient\ClientInterface;
 use Phpro\SoapClient\Event\RequestEvent;
+use Phpro\SoapClient\Event\ResponseEvent;
 use Phpro\SoapClient\Type\RequestInterface;
+use Phpro\SoapClient\Type\ResultInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Stopwatch\StopwatchEvent;
 
 class SoapCallSpec extends ObjectBehavior
 {
@@ -37,5 +41,43 @@ class SoapCallSpec extends ObjectBehavior
         $this->getMethod()->shouldBe('soapMethod');
         $this->getRequest()->shouldBe($request);
         $this->getHash()->shouldStartWith('Double\ClientInterface');
+    }
+
+    function it_should_stop_request(
+        Stopwatch $stopwatch,
+        ResponseEvent $responseEvent,
+        RequestEvent $requestEvent,
+        ResultInterface $result,
+        Client $client
+    ) {
+        $responseEvent->getResponse()->willReturn($result);
+        $responseEvent->getClient()->willReturn($client);
+        $stopwatch->start(Argument::type('string'))->shouldBeCalled();
+        $stopwatch->stop(Argument::type('string'))->shouldBeCalled();
+
+        $this->setRequest($requestEvent);
+        $this->setResponse($responseEvent);
+    }
+
+    function it_should_parse_the_debug_array(
+        ResponseEvent $responseEvent,
+        RequestEvent $requestEvent,
+        ResultInterface $result,
+        Client $client
+    ) {
+        $debugData = [
+            'request'  => ['headers' => 'SoapRequestHeader', 'body' => '<?xml version="1.0" encoding="UTF-8"?><body>SoapRequestBody</body>'],
+            'response' => ['headers' => 'SoapResponseHeader', 'body' => '<?xml version="1.0" encoding="UTF-8"?><body>SoapResponseBody</body>'],
+        ];
+        $client->debugLastSoapRequest()->willReturn($debugData);
+        $responseEvent->getResponse()->willReturn($result);
+        $responseEvent->getClient()->willReturn($client);
+
+        $this->setRequest($requestEvent);
+        $this->setResponse($responseEvent);
+        $this->getResponseHeaders()->shouldBe($debugData['response']['headers']);
+        $this->getResponseBody()->shouldStartWith('<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL . '<body>SoapResponseBody</body>');
+        $this->getRequestHeaders()->shouldBe($debugData['request']['headers']);
+        $this->getRequestBody()->shouldStartWith('<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL . '<body>SoapRequestBody</body>');
     }
 }
